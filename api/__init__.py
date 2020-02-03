e6d97f78af45535d64b63f7b505466e9cdcba033
ekoly <ecowley@protonmail.com>
2020-02-03 16:38:46 -0500

    update ipynb

--- api/__init__.py
+++ api/__init__.py
@@ -6,8 +6,10 @@ from decouple import config
 from dotenv import load_dotenv
 
 from flask import Flask 
+from dash import Dash
 
 APP = Flask(__name__)
+DASH = Dash(__name__, server=APP)
 LOG = logging.getLogger("optimal-price")
 logging.basicConfig(level=logging.DEBUG)
 
